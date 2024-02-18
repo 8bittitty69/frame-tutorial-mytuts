2c05f5fe376b0fbeb7c2f49a73ac9aa780b1806a
8bittitty69 <159496504+8bittitty69@users.noreply.github.com>
2024-02-17 18:14:18 -0700

    Update page.tsx

--- app/twitterx/page.tsx
+++ app/twitterx/page.tsx
@@ -1,3 +1,33 @@
+import { getFrameMetadata } from '@coinbase/onchainkit';
+import type { Metadata } from 'next';
+
+const frameMetadata = getFrameMetadata({
+  buttons: [
+    {
+      label: "Begin"
+    }
+  ],
+  image: `${process.env.NEXT_PUBLIC_GATEWAY_URL}/ipfs/QmP3encMRpyDwqHTKBA73CzHTtk1xUiYGtbD3MsS6hAHUi/0.png`,
+  post_url: `${process.env.NEXT_PUBLIC_BASE_URL}/api/frame?id=1`,
+});
+
+export const metadata: Metadata = {
+  title: 's3xysh1t',
+  description: 'A frame telling the story of s3xysh1t',
+  openGraph: {
+    title: 'Cosmic Cowboys',
+    description: 'A frame telling the story of s3xysh1t',
+    images: [`${process.env.NEXT_PUBLIC_GATEWAY_URL}/ipfs/QmP3encMRpyDwqHTKBA73CzHTtk1xUiYGtbD3MsS6hAHUi/0.png`],
+  },
+  other: {
+    ...frameMetadata,
+  },
+};
+
 export default function Page() {
-    return <h1>Redirecting...</h1>
-  }
+  return (
+    <>
+      <h1>s3xysh1t</h1>
+    </>
+  );
+}
