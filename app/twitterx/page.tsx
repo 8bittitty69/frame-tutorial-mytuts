902ade8dabbc82cde8fccb3c6ae131f3bf5570bb
8bittitty69 <159496504+8bittitty69@users.noreply.github.com>
2024-02-17 20:00:41 -0700

    Update page.tsx

--- app/twitterx/page.tsx
+++ app/twitterx/page.tsx
@@ -1,33 +1,4 @@
-import { getFrameMetadata } from '@coinbase/onchainkit';
-import type { Metadata } from 'next';
-
-const frameMetadata = getFrameMetadata({
-  buttons: [
-    {
-      label: "Begin"
-    }
-  ],
-  image: `${process.env.NEXT_PUBLIC_GATEWAY_URL}/ipfs/QmVf8be8nZGLMBPTDqsLFAft7PgD3HLoXr2vAgfbJ4WYzw/0.png`,
-  post_url: `${process.env.NEXT_PUBLIC_BASE_URL}/api/frame?id=1`,
-});
-
-export const metadata: Metadata = {
-  title: 's3xysh1t',
-  description: 'A frame telling the story of s3xysh1t',
-  openGraph: {
-    title: 's3xysh1t',
-    description: 'A frame telling the story of s3xysh1t',
-    images: [`${process.env.NEXT_PUBLIC_GATEWAY_URL}/ipfs/QmVf8be8nZGLMBPTDqsLFAft7PgD3HLoXr2vAgfbJ4WYzw/0.png`],
-  },
-  other: {
-    ...frameMetadata,
-  },
-};
-
 export default function Page() {
-  return (
-    <>
-      <h1>s3xysh1t</h1>
-    </>
-  );
-}
+    return <h1>Redirecting...</h1>
+  }
+  
