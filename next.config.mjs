85c27b992b1fb9a4199f53328ccc241df57a1528
8bittitty69 <159496504+8bittitty69@users.noreply.github.com>
2024-02-17 17:58:08 -0700

    new repo
    testing

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,20 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    reactStrictMode: true,
+    async redirects(){
+      return [
+        {
+          source: '/sexyshit',
+          destination: 'https://gallery.manifold.xyz/pornomusic',
+          permanent: false
+        },
+        {
+          source: '/twitterx',
+          destination: 'https://twitter_8bit_titty',
+          permanent: false
+        }
+      ]
+    }
+  };
+  
+  export default nextConfig;
