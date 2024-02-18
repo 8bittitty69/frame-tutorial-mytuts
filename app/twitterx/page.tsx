de268793f3d5b7524c43a4d38ff63b7c580b044b
8bittitty69 <159496504+8bittitty69@users.noreply.github.com>
2024-02-17 19:08:05 -0700

    Update page.tsx

--- app/twitterx/page.tsx
+++ app/twitterx/page.tsx
@@ -7,7 +7,7 @@ const frameMetadata = getFrameMetadata({
       label: "Begin"
     }
   ],
-  image: `${process.env.NEXT_PUBLIC_GATEWAY_URL}/ipfs/QmP3encMRpyDwqHTKBA73CzHTtk1xUiYGtbD3MsS6hAHUi/0.png`,
+  image: `${process.env.NEXT_PUBLIC_GATEWAY_URL}/ipfs/QmVf8be8nZGLMBPTDqsLFAft7PgD3HLoXr2vAgfbJ4WYzw/0.png`,
   post_url: `${process.env.NEXT_PUBLIC_BASE_URL}/api/frame?id=1`,
 });
 
@@ -17,7 +17,7 @@ export const metadata: Metadata = {
   openGraph: {
     title: 's3xysh1t',
     description: 'A frame telling the story of s3xysh1t',
-    images: [`${process.env.NEXT_PUBLIC_GATEWAY_URL}/ipfs/QmP3encMRpyDwqHTKBA73CzHTtk1xUiYGtbD3MsS6hAHUi/0.png`],
+    images: [`${process.env.NEXT_PUBLIC_GATEWAY_URL}/ipfs/QmVf8be8nZGLMBPTDqsLFAft7PgD3HLoXr2vAgfbJ4WYzw/0.png`],
   },
   other: {
     ...frameMetadata,
